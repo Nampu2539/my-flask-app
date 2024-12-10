dbb2d2ddd3ceef49b15691e870bb9133a8bfc8f9
Nampu2539 <supawitpibal@gmail.com>
2024-12-11 02:38:44 +0700

    Set default SECRET_KEY for Flask

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # Configuration for MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '66102010187supawit'
-app.config['MYSQL_DB'] = 'healthycal'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
